f7d543f67ad40b838dabb5b882f46f2b4a0e96e0
Sabeen Syed <sabeen.syed@rackspace.com>
2019-03-02 00:19:29 -0600

    Move menu links to the right

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,23 +1,16 @@
 import React from 'react'
 import { Link } from "gatsby"
 
+import './menu.css';
+
 const Menu = () => (
-    <div style={{
-        background: '#f4f4f4',
-        paddingTop: '10px'
-    }}>
-
-    <ul style={{
-        listStyle: 'none',
-        display: 'flex',
-        justifyContent: 'space-evenly',
-    }}>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/blog">Blog</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-
-    </ul>
-    </div>
+        <div>
+            <ul>
+                <li><Link to="/contact">Contact</Link></li>
+                <li><Link to="/blog">Blog</Link></li>
+                <li><Link to="/">Home</Link></li>
+            </ul>
+        </div>
 )
 
 export default Menu;
