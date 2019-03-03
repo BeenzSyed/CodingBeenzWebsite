d49331492c7d2a1a28afde483a7f97d2ba63c9fe
Sabeen Syed <sabeen.syed@rackspace.com>
2019-03-03 00:24:53 -0600

    Add the ability to create blog posts

---
+++ src/pages/2019-03-02/index.md
@@ -0,0 +1,8 @@
+---
+path: "/post-three"
+date: "2019-03-02"
+title: "My Third Post"
+author: "CodingBeenz"
+---
+
+This is the third blog post on my CodingBeenz website!
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,15 +1,48 @@
 import React from 'react'
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 
-const BlogPage = () => {
+const BlogPage = ({data}) => {
     return (
         <Layout>
         <div>
-            <p>Blog goes here..</p>
+            <h1>Latest Post</h1>
+            {data.allMarkdownRemark.edges.map(post => (
+                <div key={post.node.id}>
+                    <h3>{post.node.frontmatter.title}</h3>
+                    <small>Posted By {post.node.frontmatter.author} on {post.node.frontmatter.date}</small>
+                    <br />
+                    <br />
+                    <Link to={post.node.frontmatter.path}>Read More</Link>
+                    <br />
+                    <br />
+                    <hr />
+                </div>
+            ))}
         </div>
         </Layout>
     )
 }
+
+export const pageQuery = graphql`
+    query BlogIndexQuery {
+        allMarkdownRemark(
+            sort: {order:DESC, fields: frontmatter___date}
+        ){
+            edges {
+                node {
+                    id
+                    frontmatter {
+                        path
+                        title
+                        date
+                        author
+                    }
+                }
+            }
+        }
+    }
+`
 
 export default BlogPage
