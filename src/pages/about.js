b412c3b0179285eeff97ce59d4d0f26b41f6f4e4
BeenzSyed <sabeen.kazmi@gmail.com>
2019-04-08 09:44:27 -0500

    Add slug code

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,13 @@
-import React from 'react'
+import React from "react"
 
 import Layout from "../components/layout"
 
-
 const AboutPage = () => {
-    return (
-        <Layout>
-            <p>
-                This page will tell you about me
-            </p>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <p>This page will tell you about me</p>
+    </Layout>
+  )
 }
 
 export default AboutPage
