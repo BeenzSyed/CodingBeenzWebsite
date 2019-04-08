fed291ca50a576014e9f58cdedb9de1352072570
BeenzSyed <sabeen.kazmi@gmail.com>
2019-04-08 12:51:23 -0500

    Add small blurb to About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,18 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 
 const AboutPage = () => {
   return (
     <Layout>
-      <p>This page will tell you about me</p>
+      <p class="about">
+        Hi! I'm Sabeen and I love teaching and writing about Computer Science
+        concepts. Check out my <Link to="/blog">Blog</Link>,{" "}
+        <a href="https://www.instagram.com/codingbeenz/">Instagram</a> and{" "}
+        <a href="https://twitter.com/CodingBeenz">Twitter</a> where I post
+        regularly!
+      </p>
     </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,17 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 import StyledBackgroundSection from "../components/backgroundimage"
-import Menu from "../components/menu"
-
 
 const IndexPage = ({ data }) => (
   <>
-  <StyledBackgroundSection>
+    <StyledBackgroundSection>
       <Layout>
-      <SEO title="Home" keywords={[`computer science`, `coding`, `gatsby`]} />
+        <SEO title="Home" keywords={[`computer science`, `coding`, `gatsby`]} />
       </Layout>
     </StyledBackgroundSection>
   </>
-);
+)
 
-export default IndexPage;
+export default IndexPage
