50073f479ca3c3ee0a137594586b698d9f05cdbf
BeenzSyed <sabeen.kazmi@gmail.com>
2019-04-11 22:24:28 -0500

    Add Merriweather font

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,17 +35,10 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
-      // resolve: `gatsby-plugin-google-fonts`,
-      // options: {
-      //   fonts: [
-      //     `Open Sans`,
-      //     `VT323`,
-      //     {
-      //       family: `Montserrat`,
-      //       variants: [`400`, `900`],
-      //     },
-      //   ],
-      // },
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`Merriweather`],
+      },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
