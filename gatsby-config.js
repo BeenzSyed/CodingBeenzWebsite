c9cfdcbace269fadbd5f924101f23efaab320202
BeenzSyed <sabeen.kazmi@gmail.com>
2019-04-19 14:41:05 -0500

    Add gatsby-remark-copy-linked-files for pdf file

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,12 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: ["gatsby-remark-copy-linked-files"],
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
